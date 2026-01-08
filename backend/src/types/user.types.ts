c867176126f9b2640f47e803e5404c0b995671e1
Lucas Hernández <lucas.hernandezu@gmail.com>
2026-01-08 14:35:01 -0300

    Register de usuarios v1

---
+++ backend/src/types/user.types.ts
@@ -0,0 +1,50 @@
+import { Role } from '@prisma/client'
+
+export interface RegisterUserDTO {
+    email: string;
+    password: string;
+    role: Role;
+    first_name: string;
+    last_name: string;
+    phone: string;
+    rut: string;
+}
+
+export interface LoginUserDTO {
+    email: string;
+    password: string;
+}
+
+export interface UpdateUserDTO {
+    first_name?: string;
+    last_name?: string;
+    phone?: string;
+}
+
+export interface ChangePasswordDTO {
+  current_password: string;
+  new_password: string;
+}
+
+export interface ChangeEmailDTO {
+  new_email: string;
+  password: string;
+}
+
+export interface UserResponse {
+    id: number;
+    email: string;
+    role: Role;
+    first_name: string;
+    last_name: string;
+    phone: string;
+    rut: string;
+    is_active: boolean;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface AuthResponse {
+    user: UserResponse;
+    token: string;
+}
